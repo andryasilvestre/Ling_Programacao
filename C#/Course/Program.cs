7dfaaa5f269d87cece517a79a25dd0e422546313
Ândrya Silvestre <andryasilvestre@gmail.com>
2022-09-28 07:08:18 -0300

    add info no program.cs

--- C#/Course/Program.cs
+++ C#/Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course {
     class Program {
@@ -16,6 +17,7 @@ namespace Course {
             string nome = "Maggie May";
             sbyte nx = sbyte.MinValue;
             decimal nz = decimal.MaxValue;
+            double saldo = 10.434332;
             
             Console.WriteLine(completo);
             System.Console.WriteLine(letra);
@@ -32,7 +34,24 @@ namespace Course {
             System.Console.WriteLine("\n\n");
 
             System.Console.WriteLine("Teste");
-            System.Console.Write("Bom dia");
+            System.Console.Write("Bom dia");    // write não pula linha. wrileline pula
+            System.Console.WriteLine("Teste1");
+
+            System.Console.WriteLine("-----------------\n");
+
+            System.Console.WriteLine(saldo.ToString("F2")); // F2 - 2 casas decimais; F3 - 3 casas...
+            System.Console.WriteLine(saldo.ToString("F4"));
+            System.Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); // desconsidera qqr formatacao específica de qqr país
+
+            // nome e genero
+            //placeholder
+            System.Console.WriteLine("{0} é do sexo {1}.", nome, genero);
+
+            //interpolação - mais recente
+            System.Console.WriteLine($"{nome} é do sexo {genero}. A ração dela custa R$ {saldo:F2}.");
+
+            //Concatenação - mais antigo
+            System.Console.WriteLine(nome + " é do sexo " + genero + ". A ração dela custa R$ " + (saldo.ToString("F2", CultureInfo.InvariantCulture)));
         } 
     }
 
